e30db140a732cfad7d805373e0a94c505e47c83a
wasiowision <wasi@owision.com>
2025-01-12 16:41:59 +0530

    Implement note details page with dynamic data loading and update notes.json with new entries

--- app/routes/newnote.$id.jsx
+++ app/routes/newnote.$id.jsx
@@ -1,8 +1,19 @@
+import { useLoaderData } from "@remix-run/react"
+import { getStoredNotes } from "../data/notes"
 
 export default function NoteDetailsPage() {
+    const data = useLoaderData()
     return (
         <main id="note-details">
-            <h1>Note Title, Note Content</h1>
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
         </main>
     )
+}
+
+export async function loader({ params }) {
+    const { id } = params
+    const allentries = await getStoredNotes()
+    const speceficEntry = allentries.find((entry) => entry.id == id)
+    return speceficEntry
 }
